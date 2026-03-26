3f1d67e3f22253027737517a5ff9df3603f03049
Shantanu Pawar <pawarshantanu4318@gmail.com>
2026-03-26 13:40:53 +0530

    jsx file added by shantanu
    jsx file added by shantanu

---
+++ Demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Demo = () => {
+  return (
+    <div>Demo
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores minima at explicabo quia modi fuga suscipit repellat error nihil laborum, quos nesciunt minus recusandae sed, commodi veritatis dolor illum provident.</p>
+    </div>
+  )
+}
+
+export default Demo 
